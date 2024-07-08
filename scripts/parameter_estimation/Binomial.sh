be37cbfb9bdae910a4c149e1fabf327afe838a99
Brainiaccc <52440972+jason51108@users.noreply.github.com>
2024-07-08 23:40:07 +0800

    Update Binomial.sh

--- scripts/parameter_estimation/Binomial.sh
+++ scripts/parameter_estimation/Binomial.sh
@@ -2,12 +2,12 @@
 params=(500 1000 2000 4000 8000)
 
 for param in "${params[@]}"; do
-    for ((i=1; i<=2; i++)); do
+    for ((i=1; i<=200; i++)); do
         number=$i;
         export number
         python3 -u /home/user/CYH/Code_For_MDS/Project/run.py \
                 --task_name 'parameter estimation' \
-                --type distance2 \
+                --type distance1 \
                 --model Binomial \
                 --seed_number $i \
                 --num_samples $param \
@@ -16,7 +16,7 @@ for param in "${params[@]}"; do
                 --data Simulation \
                 --learning_rate 1 \
                 --patience relative \
-                --tolerace 0.1 &
+                --tolerace 0.00000001 &
     done
 done
 wait
