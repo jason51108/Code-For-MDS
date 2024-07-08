aeb5eaee47025a7af6173a74b8f5e63e6fdf21ca
Brainiaccc <52440972+jason51108@users.noreply.github.com>
2024-07-08 23:40:09 +0800

    Update exp_parameter_estimation.py

--- exp/exp_parameter_estimation.py
+++ exp/exp_parameter_estimation.py
@@ -30,14 +30,6 @@ class Exp_Parameter_Estimation(Exp_Basic):
 
     # train
     def train(self):
-        start = time.time()
-        pred_alpha, pred_theta, iter = self.model.train(self.adjacency_matrix) # start training
-        end = time.time()
-        elapsed_time = end - start
-        hours, rem = divmod(elapsed_time, 3600)
-        minutes, seconds = divmod(rem, 60)
-        print(f'n:{self.args.num_samples}, number:{self.args.seed_number}, iter:{iter}', f'time:{int(hours)}h {int(minutes)}m {seconds:.2f}s')
-        
         # create folder
         if self.args.model == 'Normal':
             folder_path =  (f'/home/user/CYH/Code_For_MDS/Project/para_result/{self.args.task_name}/'
@@ -51,14 +43,25 @@ class Exp_Parameter_Estimation(Exp_Basic):
                 f'{self.args.constrain}_{self.args.dimension}_{self.args.learning_rate}'
                 f'{self.args.patience}_{self.args.tolerace}/'
                 f'n_{self.args.num_samples}/{self.args.seed_number}/')
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
         
-        # save data
-        np.save(folder_path + f'pred_alpha.npy', pred_alpha) if self.args.type != 'distance1' else None
-        np.save(folder_path + f'pred_theta.npy', pred_theta) 
-        np.save(folder_path + f'true_alpha.npy', self.true_alpha) if self.args.type !='distance1' and self.args.data != 'Custom' else None
-        np.save(folder_path + f'true_theta.npy', self.true_theta) if self.args.data != 'Custom' else None
-        np.save(folder_path + f'adjacency_matrix.npy', self.adjacency_matrix)
+        # create dir
+        os.makedirs(folder_path, exist_ok=True)
+        if len(os.listdir(folder_path)) == 0:
+            # train
+            start = time.time()
+            pred_alpha, pred_theta, iter = self.model.train(self.adjacency_matrix) # start training
+            end = time.time()
+            elapsed_time = end - start
+            hours, rem = divmod(elapsed_time, 3600)
+            minutes, seconds = divmod(rem, 60)
+            print(f'n:{self.args.num_samples}, number:{self.args.seed_number}, iter:{iter}', f'time:{int(hours)}h {int(minutes)}m {seconds:.2f}s')
+            # save data
+            np.save(folder_path + f'pred_alpha.npy', pred_alpha) if self.args.type != 'distance1' else None
+            np.save(folder_path + f'pred_theta.npy', pred_theta) 
+            np.save(folder_path + f'true_alpha.npy', self.true_alpha) if self.args.type !='distance1' and self.args.data != 'Custom' else None
+            np.save(folder_path + f'true_theta.npy', self.true_theta) if self.args.data != 'Custom' else None
+            np.save(folder_path + f'adjacency_matrix.npy', self.adjacency_matrix)
+        else:
+            pass
         
         return None
